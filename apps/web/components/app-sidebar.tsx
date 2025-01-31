a78c78a3df86ffaddeeffd7215edac8a4a610053
Pakorn Vongseela <18078609+pakornv@users.noreply.github.com>
2025-01-31 15:46:59 +0700

    feat(web): add AppSidebar component

---
+++ apps/web/components/app-sidebar.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import { Link } from "@repo/ui/link";
+import { cn } from "@repo/ui/utils";
+import { usePathname } from "next/navigation";
+
+type AppSidebarProps = {
+  items: {
+    name: string;
+    href: string;
+    icon: React.ElementType;
+  }[];
+};
+
+function AppSidebar({ items }: AppSidebarProps) {
+  const pathName = usePathname();
+
+  return (
+    <nav className="grow overflow-y-auto px-4 py-8">
+      <ul className="flex flex-col gap-y-1">
+        {items.map((item) => (
+          <li key={item.href}>
+            <Link
+              href={item.href}
+              className={cn(
+                "group flex gap-x-3 rounded-md px-3 py-2 font-medium text-ui-green-500 hover:font-extrabold focus-visible:outline-0",
+                pathName.startsWith(item.href) && "font-extrabold",
+              )}
+            >
+              <item.icon
+                aria-hidden="true"
+                className={cn(
+                  "size-6 stroke-[1.5] group-data-hovered:stroke-2",
+                  pathName.startsWith(item.href) && "stroke-2",
+                )}
+              />
+              {item.name}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+}
+
+export { AppSidebar };
